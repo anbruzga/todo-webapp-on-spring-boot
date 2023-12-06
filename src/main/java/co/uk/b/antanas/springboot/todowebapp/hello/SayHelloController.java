5adc11a17b921944ff2b057ace2355f80725da09
Antanas Bruzga <bruzgadev@gmail.com>
2023-12-06 22:18:11 +0000

    Added 3 Say Hello methods: via JSP, via embedded html, via simple string

--- src/main/java/co/uk/b/antanas/springboot/todowebapp/hello/SayHelloController.java
+++ src/main/java/co/uk/b/antanas/springboot/todowebapp/hello/SayHelloController.java
@@ -14,4 +14,38 @@ public class SayHelloController {
     public String sayHello() {
         return "Hello! What are you learning today?";
     }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        String html = """
+                <!DOCTYPE html>
+                <html lang="en">
+                  <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <title>My Hello App</title>
+                    <link rel="stylesheet" href="./style.css">
+                    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+                  </head>
+                  <body>
+                    <main>
+                        <h1>Welcome to My Website</h1> \s
+                    </main>
+                	<script src="index.js"></script>
+                  </body>
+                </html>
+                """;
+
+        return html;
+    }
+
+
+    // /say-hello-jsp => sayHello.jsp
+    // src/main/webapp/WEB-INF/jsp/sayHello.jsp
+    @RequestMapping("say-hello-jsp")
+    public String sayHelloJsp() {
+        return "sayHello";
+    }
 }
