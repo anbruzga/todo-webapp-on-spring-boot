37abb2e451e390a7eb1d038cbc8297d9a177d946
Antanas Bruzga <bruzgadev@gmail.com>
2023-12-10 15:12:34 +0000

    Add H2 Database and Enable Filter Chain in Spring Security so I can access H2 Console

--- src/main/java/co/uk/b/antanas/springboot/todowebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/co/uk/b/antanas/springboot/todowebapp/security/SpringSecurityConfiguration.java
@@ -2,13 +2,19 @@ package co.uk.b.antanas.springboot.todowebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -39,6 +45,22 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // This redefines the security sequence....
+    // All URLs are protected
+    // A login form is shown for unauthorized requests
+    // CSRF disable - Cross Site Request Forging -- to access H2
+    // Allow Frames (Html in H2 makes use of Frames, that are not allowed by default in Spring Security
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        return http.build();
     }
 
 
